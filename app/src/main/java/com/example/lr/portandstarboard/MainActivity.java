8babe71ce0832cea3d95c59ab809958101999db1
Rong Li <lirongl@sfu.ca>
2018-01-17 17:05:28 -0800

    Second commit

--- app/src/main/java/com/example/lr/portandstarboard/MainActivity.java
+++ app/src/main/java/com/example/lr/portandstarboard/MainActivity.java
@@ -5,19 +5,25 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Game guessGame = new Game();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        updateUI();
         // ..get the Button  test
         Button btn1 = (Button) findViewById(R.id.leftBtnID);
         Button btn2 = (Button) findViewById(R.id.rightBtnID);
         // ..set what happens after clicked
+
+        Button btn3 = (Button) findViewById(R.id.meansLeft);
+        Button btn4 = (Button) findViewById(R.id.meansRight);
+
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -32,5 +38,48 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("MyApp","Starboard (right) is green");
             }
         });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String endMessage;
+                Game.Side userGuess = Game.Side.PORT;
+                if (guessGame.checkIfCorrect(userGuess)) {
+                    endMessage = "Correct!";
+                    Log.i("MyApp", "“User guess of Port was Correct!");
+                } else {
+                    endMessage = "Incorrect!";
+                    Log.i("MyApp", "“User guess of Port was Incorrect!");
+
+                }
+                Toast.makeText(getApplicationContext(), endMessage, Toast.LENGTH_SHORT).show();
+                updateUI();
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String endMessage;
+                Game.Side userGuess = Game.Side.STARBOARD;
+                if (guessGame.checkIfCorrect(userGuess)) {
+                    endMessage = "Correct!";
+                    Log.i("MyApp", "“User guess of Port was Correct!");
+                } else {
+                    endMessage = "Incorrect!";
+                    Log.i("MyApp", "“User guess of Port was Incorrect!");
+                }
+                Toast.makeText(getApplicationContext(), endMessage, Toast.LENGTH_SHORT).show();
+                updateUI();
+            }
+        });
+
     }
+    private void updateUI(){
+        guessGame = new Game();
+        TextView textview = (TextView) findViewById(R.id.sideID);
+        String side = guessGame.getChosenSideName();
+        textview.setText(side);
+    }
+    //
+    //
 }
